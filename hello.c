10449f8eb5fa0a9ed0e23c0434405190b6e5462c
Alex Ventura <aventura3@huskers.unl.edu>
2024-09-03 13:30:56 -0500

    Added Alex Ventura (LA)

--- hello.c
+++ hello.c
@@ -9,5 +9,8 @@ int main(int argc, char **argv) {
     printf("hello\n");
     printf("pre-med minor\n");
 
+    printf("Hello my name is Alex Ventura (LA)\n");
+    printf("My major is Computer Engineering\n");
+
     return 0;
 }
